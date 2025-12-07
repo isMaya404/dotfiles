9521d73c5201a8990611e4cd614a6e27dff6c655
isMaya404 <franciajeremiah10@gmail.com>
2025-12-07 18:54:30 +0800

    automated commit

--- .config/nvim/lua/configs/lsp.lua
+++ .config/nvim/lua/configs/lsp.lua
@@ -119,22 +119,22 @@ local servers = {
         end,
     },
 
-    jsonls = {
-        cmd = { 'vscode-json-language-server', '--stdio' },
-        filetypes = { 'json', 'jsonc' },
-        init_options = {
-            provideFormatter = true,
-        },
-        root_markers = { '.git' },
-        validate = { enable = true },
-    },
+    -- jsonls = {
+    --     cmd = { 'vscode-json-language-server', '--stdio' },
+    --     filetypes = { 'json', 'jsonc' },
+    --     init_options = {
+    --         provideFormatter = true,
+    --     },
+    --     root_markers = { '.git' },
+    --     validate = { enable = true },
+    -- },
 
-    sqls = {
-        cmd = { 'sqls' },
-        filetypes = { 'sql', 'mysql' },
-        root_markers = { 'config.yml' },
-        settings = {},
-    },
+    -- sqls = {
+    --     cmd = { 'sqls' },
+    --     filetypes = { 'sql', 'mysql' },
+    --     root_markers = { 'config.yml' },
+    --     settings = {},
+    -- },
 
     -- cssls = {
     --     cmd = { 'vscode-css-language-server', '--stdio' },
@@ -391,64 +391,64 @@ local servers = {
         end,
     },
 
-    -- vtsls = {
-    --     cmd = { 'vtsls', '--stdio' },
-    --     init_options = {
-    --         hostInfo = 'neovim',
-    --     },
-    --     filetypes = {
-    --         'javascript',
-    --         'javascriptreact',
-    --         'javascript.jsx',
-    --         'typescript',
-    --         'typescriptreact',
-    --         'typescript.tsx',
-    --     },
-    --     root_dir = function(bufnr, on_dir)
-    --         local root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock' }
-    --         -- Give the root markers equal priority by wrapping them in a table
-    --         root_markers = vim.fn.has 'nvim-0.11.3' == 1 and { root_markers, { '.git' } } or vim.list_extend(root_markers, { '.git' })
-    --
-    --         -- exclude deno
-    --         if vim.fs.root(bufnr, { 'deno.json', 'deno.lock' }) then
-    --             return
-    --         end
-    --
-    --         -- We fallback to the current working directory if no project root is found
-    --         local project_root = vim.fs.root(bufnr, root_markers) or vim.fn.getcwd()
-    --
-    --         on_dir(project_root)
-    --     end,
-    --     on_attach = function(client, bufnr)
-    --         local function map(lhs, rhs)
-    --             vim.keymap.set('n', lhs, rhs, { buffer = bufnr })
-    --         end
-    --
-    --         -- fix all
-    --         vim.keymap.set('n', 'glf', function()
-    --             vim.lsp.buf.code_action {
-    --                 apply = true,
-    --                 context = { only = { 'source.fixAll' } },
-    --             }
-    --         end)
-    --
-    --         -- add missing imports
-    --         map('gli', function()
-    --             vim.lsp.buf.code_action {
-    --                 apply = true,
-    --                 context = { only = { 'source.addMissingImports.ts' } },
-    --             }
-    --         end)
-    --
-    --         -- remove unused
-    --         map('glu', function()
-    --             vim.lsp.buf.code_action {
-    --                 apply = true,
-    --                 context = { only = { 'source.removeUnusedImports' } },
-    --             }
-    --         end)
-    --     end,
-    -- },
+    vtsls = {
+        cmd = { 'vtsls', '--stdio' },
+        init_options = {
+            hostInfo = 'neovim',
+        },
+        filetypes = {
+            'javascript',
+            'javascriptreact',
+            'javascript.jsx',
+            'typescript',
+            'typescriptreact',
+            'typescript.tsx',
+        },
+        root_dir = function(bufnr, on_dir)
+            local root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock' }
+            -- Give the root markers equal priority by wrapping them in a table
+            root_markers = vim.fn.has 'nvim-0.11.3' == 1 and { root_markers, { '.git' } } or vim.list_extend(root_markers, { '.git' })
+
+            -- exclude deno
+            if vim.fs.root(bufnr, { 'deno.json', 'deno.lock' }) then
+                return
+            end
+
+            -- We fallback to the current working directory if no project root is found
+            local project_root = vim.fs.root(bufnr, root_markers) or vim.fn.getcwd()
+
+            on_dir(project_root)
+        end,
+        on_attach = function(client, bufnr)
+            local function map(lhs, rhs)
+                vim.keymap.set('n', lhs, rhs, { buffer = bufnr })
+            end
+
+            -- fix all
+            vim.keymap.set('n', 'glf', function()
+                vim.lsp.buf.code_action {
+                    apply = true,
+                    context = { only = { 'source.fixAll' } },
+                }
+            end)
+
+            -- add missing imports
+            map('gli', function()
+                vim.lsp.buf.code_action {
+                    apply = true,
+                    context = { only = { 'source.addMissingImports.ts' } },
+                }
+            end)
+
+            -- remove unused
+            map('glu', function()
+                vim.lsp.buf.code_action {
+                    apply = true,
+                    context = { only = { 'source.removeUnusedImports' } },
+                }
+            end)
+        end,
+    },
 
     denols = {
         cmd = { 'deno', 'lsp' },
